65748d93ac6fe370760efd8f9ee7e8e73327c0d2
RonitNayak25 <ronitnayak25@gmail.com>
2020-10-04 02:40:51 +0530

    Initial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import { CircularProgress } from "@material-ui/core";
+import "./App.css";
+import axios from 'axios';
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [inputPercent, setInputPercent] = useState("");
+  const [play, setPlay] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  async function handleSubmit (event){
+    event.preventDefault();
+    setLoading(true);
+    setSuccess(false);
+    try{
+      const res = await axios.post(`/predict`, { "text": inputText, "percent": inputPercent });
+      console.log(res.data);
+      console.log(inputText);
+      setPlay(res.data.prediction);
+      setLoading(false);
+      setSuccess(true);
+    }
+    catch(e){
+      setLoading(false);
+      console.log(e)
+    }
+  }
+
+  const handleTextChange = event => {
+    setInputText(event.target.value);
+  }
+  const handlePercentChange = event => {
+    setInputPercent(event.target.value);
+  }
+
+  let id = 0; // acts as a key for <p></p> mapping
+
+  return (
+    <div className="App">
+      <div className="container">
+        <br />
+        <br />
+        <div className="row">
+          <div className="col-md-12">
+            <form className="form-group" method="POST" onSubmit={handleSubmit}>
+              <input
+                type="textarea"
+                className="form-control"
+                id="text"
+                name="text"
+                onChange={handleTextChange}
+              />
+              <input
+                type="text"
+                className="form-control"
+                id="percent"
+                name="percent"
+                onChange={handlePercentChange}
+              />
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </form>
+            <br />
+            {loading ? (
+              <CircularProgress color="secondary" />
+            ) : (
+              <div className="jumbotron jumbotron-fluid">
+                <div className="container">
+                  <h1 className="display-4">
+                    <u>Summary</u>
+                  </h1>
+                  {success ? <div className="lead" style={{ textAlign: "left" }}>
+                    {play.split("\n").map((i) => {
+                      return <p key={id++}>{i}</p>;
+                    })}
+                  </div> : <div></div> }
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ server.py
@@ -0,0 +1,33 @@
+import os
+from flask import Flask,request,render_template
+import flask
+from flask.helpers import send_from_directory
+from flask.json import jsonify
+from model import summarize
+import os
+
+app = Flask(__name__, static_folder='frontend/build/static',template_folder="frontend/build")
+
+@app.route('/')
+def hello():
+    return render_template("index.html")
+
+@app.route('/predict',methods=['GET','POST'])
+def predict():
+    if request.method == 'POST':
+        text = request.get_json()
+        print(text.get('hi'))
+        op = text.get('text')
+        percent = int(text.get('percent'))
+        prediction = summarize(op, percent)
+        return jsonify({"prediction":prediction})
+    else:
+        return 'Not Authorised.'
+
+if __name__ == "__main__":
+    app.secret_key = 'ltsmgrurip'
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
+
+
+# app = Flask(__name__, static_folder='../build', static_url_path='/')
